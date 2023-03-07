9e9b6f2cd77a008fc9a64ea1ee052eccb1cdabdc
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-07 14:40:48 -0300

    feat: add GET /transactions/:id route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -27,6 +27,18 @@ export async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
     const transactions = await knex('transactions').select('*')
 
-    return transactions
+    return { transactions }
+  })
+
+  app.get('/:id', async (request, reply) => {
+    const getTransactionParamSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
   })
 }
